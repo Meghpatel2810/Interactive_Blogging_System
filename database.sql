819595fec9e193bceb1c106cf922c54a62bfe591
MeghPatel23CS069 <23cs069@charusat.edu.in>
2025-03-01 22:52:17 +0530

    Updated database

--- database.sql
+++ database.sql
@@ -7,8 +7,29 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- truncate table users ;
--- 
--- drop table demo ;
--- 
--- select * from users ;
+create table post (
+	post_id int auto_increment primary key,
+    id int ,
+    foreign key(id) references users(id) on delete cascade ,
+    title varchar(255) not null,
+    content text not null,
+    created_at timestamp default current_timestamp
+);
+
+create table post_likes (
+	id int not null,
+	post_id int not null,
+    primary key(id,post_id),
+    foreign key(id) references users(id) on delete cascade,
+    foreign key(post_id) references post(post_id) on delete cascade
+);
+
+create table comments (
+	comment_id int auto_increment primary key,
+	id int not null,
+    post_id int not null,
+    content text not null,
+    created_at timestamp default current_timestamp ,
+    foreign key(id) references users(id) on delete cascade,
+    foreign key(post_id) references post(post_id) on delete cascade
+);
